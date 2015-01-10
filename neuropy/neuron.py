e7989405ea61dec04e7f57d7e8c6f1ea7dbcac56
Bernat Guillén <bguillen@gmail.com>
2015-01-10 00:27:42 -0500

    Fixed default parameters and optimized speed
    Some unused methods in the class Model.

--- neuropy/neuron.py
+++ neuropy/neuron.py
@@ -1,11 +1,12 @@
 import numpy as np
+from math import tanh,cosh
 from scipy.integrate import ode
-import warnings
+
 """Environ holds all the 'global' variables such as C, gK, gCa, etc.
 gKS, Iext are the parameters that matter"""
 class Environ(object):
 
-    def __init__(self, gK = 9.0 , gCa=4.4, gL = 2.0, gKS=0.25, EK=-80., ECa=120., EL=-60., VCa=-1.2, kCa=0.11, VK=2., kK=0.2, kc=0.8, Vc=-27., C=1.2, Iext=35.6, de=0.052, ep=4.9):
+    def __init__(self, gK = 9.0 , gCa=4.4, gL = 2.0, gKS=0.19, EK=-80., ECa=120., EL=-60., VCa=-1.2, kCa=2./18., VK=2., kK=2./10., kc=0.7, Vc=-25., C=2.4, Iext=37., de=0.052, ep=2.5):
         self.gK = gK
         self.gCa = gCa
         self.gL = gL
@@ -23,7 +24,11 @@ class Environ(object):
         self.Iext = Iext
         self.ep = ep
         self.de = de
-
+        self.V1 = VCa
+        self.V2 = 2./kCa
+        self.V3 = VK
+        self.V4 = 2/kK
+        #note: kK = 2/V4 as in the MATLAB code, VCa = V1, kCa = 2/V2, V3 = VK, Vc kc = 2*kc equal
 """Model represents the model in Ghiglia, Holmes paper:
 Cv' = - [ICa + IK + IL + IKS] + Iext
 m' = ep/tm(v)*(mInf(v)-m)
@@ -43,34 +48,34 @@ class Model(object):
         return 0.5*(1+np.tanh((V-self.env.VCa)*self.env.kCa/2))
 
     def mInf(self,V):
-        return 0.5*(1+np.tanh((V-self.env.VK)*self.env.kK/2))
+        return 0.5*(1+np.tanh((V-self.env.VK)*self.env.kK))
 
     def tm(self,V):
-        return 1./np.cosh((V-self.env.VK)*self.env.kK)
+        return 1./np.cosh((V-self.env.VK)*self.env.kK/4)
 
     def cInf(self,V):
         return 0.5*(1+np.tanh(self.env.kc/2*(V-self.env.Vc)))
 
     def tc(self,V):
-        return 1./np.cosh((V-self.env.Vc)*self.env.kc)
-
+        return 1./np.cosh((V-self.env.Vc)*self.env.kK/4)
+    
     def dyn(self,t,x):
         [V, m, c] = x
-        mInf = 0.5*(1+np.tanh((V-self.env.VK)*self.env.kK/2))
-        cInf = 0.5*(1+np.tanh(self.env.kc/2*(V-self.env.Vc)))
-        nInf = 0.5*(1+np.tanh((V-self.env.VCa)*self.env.kCa/2))
-        tm = 1./np.cosh((V-self.env.VK)*self.env.kK)
-        tc = 1./np.cosh((V-self.env.Vc)*self.env.kc)
+        mInf = 0.5*(1+tanh((V-self.env.V3)/self.env.V4))
+        cInf = 0.5*(1+tanh((V-self.env.Vc)*self.env.kc))
+        nInf = 0.5*(1+tanh((V-self.env.V1)/self.env.V2))
+        #tm = 1./cosh((V-self.env.V3)/(2*self.env.V4))
+        #tc = 1./cosh((V-self.env.Vc)*self.env.kK/4)
         ICa = self.env.gCa*nInf*(V-self.env.ECa)
         IK = self.env.gK*m*(V-self.env.EK)
         IKS = self.env.gKS*c*(V-self.env.EK)
         IL = self.env.gL*(V-self.env.EL) #EK in paper
         
         Vd = -1./self.env.C*(ICa + IK + IL + IKS) + self.env.Iext/self.env.C
-        md = self.env.ep*(mInf-m)/tm
-        cd = self.env.de*(cInf-c)/tc
+        md = self.env.ep*(mInf-m)*cosh((V-self.env.V3)/(2*self.env.V4))
+        cd = self.env.de*(cInf-c)*cosh((V-self.env.Vc)/(2*self.env.V4))
         
-        return [Vd, md, cd]
+        return np.array([Vd, md, cd])
 
 
 """Neuron is an object that contains the model for V, w and c"""
@@ -85,18 +90,17 @@ class Neuron(object):
         return xd
     
     def sym(self,t0,x0,tmax,dt):
-        r = ode(self.dyn).set_integrator('vode',nsteps=1500)
-        r.set_initial_value(x0,t0)
-        #r._integrator.iwork[2] = -1
+        r = ode(self.dyn).set_integrator('dopri5')
+        r.set_initial_value(np.array(x0),t0)
         
-        t = []
-        xd = []
-       # warnings.filterwarnings("ignore",category=UserWarning)
+        t = [t0]
+        xd = [np.array(x0)]
+
         while r.successful() and r.t < tmax:
-            r.integrate(tmax, step = True)
+            r.integrate(r.t + dt)
             t.append(r.t)
             xd.append(r.y)
 #            print("%g %g %g %g" %(r.t, r.y[0],r.y[1],r.y[2]))
-     #   warnings.resetwarnings()
+
         print(r.successful())
         return [t, xd]
